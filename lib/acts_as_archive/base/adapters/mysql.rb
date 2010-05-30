b93e035dcb5e3a4ea07ebfd87937e8001eb8e41b
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2010-05-30 23:31:53 +0200

    Add multi-columns indexes support for MySQL
    refs #6
    Acts_as_archive now fully supports the following syntax :
    
    acts_as_archive :indexes => [ :id, [:subject_id, :subject_type],
    :created_at, :deleted_at ]
    
    for MySQL and Postgresql

--- lib/acts_as_archive/base/adapters/mysql.rb
+++ lib/acts_as_archive/base/adapters/mysql.rb
@@ -7,9 +7,18 @@ module ActsAsArchive
 
         def archive_table_indexed_columns
           index_query = "SHOW INDEX FROM archived_#{table_name}"
-          indexes = connection.select_all(index_query).collect do |r|
-            r["Column_name"]
+          indexes = connection.select_all(index_query)
+          final_indexes = []
+          current_index = 0
+          indexes.each do |index|
+            if index['Seq_in_index'] != '1'
+              final_indexes[current_index-1] = Array(final_indexes[current_index-1]).flatten.concat(Array(index['Column_name']))
+            else
+              final_indexes[current_index] = index['Column_name']
+              current_index += 1
+            end
           end
+          return final_indexes
         end
       end
     end
